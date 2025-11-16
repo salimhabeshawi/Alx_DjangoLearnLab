17181d17877b573467270f78cdfae635c9915397
salimhabeshawi <salimhabeshawi@gmail.com>
2025-11-16 10:00:57 +0300

    Rename LibraryDetail class to LibraryDetailView for consistency

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -12,7 +12,7 @@ def list_books(request):
     return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-class LibraryDetail(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
