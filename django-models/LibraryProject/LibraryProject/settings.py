617676cbb922e7e4baa174e6882b53092659c543
salimhabeshawi <salimhabeshawi@gmail.com>
2025-11-16 14:02:54 +0300

    Add authentication views and templates for login, logout, and registration

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = "/relationship/library/1/"
+LOGOUT_REDIRECT_URL = "/relationship/register/"
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.urls import reverse_lazy
 from django.http import HttpResponse
 from .models import Book, Library
-from .models import Library
 from django.views import View
 from django.views.generic.detail import DetailView
 
@@ -16,3 +19,31 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+# Login view
+
+
+class CustomLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+    redirect_authenticated_user = True
+
+# Logout view
+
+
+class CustomLogoutView(LogoutView):
+    pass
+
+# Registration view
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # login(request, user)
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "relationship_app/register.html", {"form": form})
