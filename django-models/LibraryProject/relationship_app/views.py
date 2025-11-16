bcb96069c5906bb5621445be5c1b120e5598820b
salimhabeshawi <salimhabeshawi@gmail.com>
2025-11-16 09:53:23 +0300

    Add list_books.html template and update views to render it for book listing

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -8,9 +8,10 @@ from django.views.generic import DetailView
 # Create your views here.
 def list_books(request):
     books = Book.objects.select_related('author').all()
-    return render(request, "relationship_app/book_list.html", {"books": books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
 
 class LibraryDetail(DetailView):
     model = Library
-    template_name = "relationship_app/library_details.html"
+    template_name = "relationship_app/list_books.html"
     context_object_name = "library"
